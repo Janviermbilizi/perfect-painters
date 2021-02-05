11b6911280eca01bf014b135f55484e929977955
Janviermbilizi <janviermbilizi@gmail.com>
2021-02-04 21:05:38 -0500

    completed MVP

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,14 +1,103 @@
-import React from "react";
+import React, { useState } from "react";
 import Title from "./Title";
 import { PhoneOutlined, SendOutlined, MailOutlined } from "@ant-design/icons";
+import { create } from "./apiCalls";
+//import axios from "axios";
 
-function Contact(props) {
+const Contact = (props) => {
+  const [values, setValues] = useState({
+    username: "",
+    phone: "",
+    email: "",
+    address: "",
+    address2: "",
+    city: "",
+    state: "",
+    zip: "",
+    message: "",
+    buttonText: "Send Message",
+    error: "",
+    success: "",
+  });
+
+  const handleChange = (name) => (event) => {
+    const value = event.target.value;
+    setValues({
+      ...values,
+      error: "",
+      success: "",
+      [name]: value,
+    });
+  };
+  const {
+    username,
+    phone,
+    email,
+    address,
+    address2,
+    city,
+    state,
+    zip,
+    message,
+    buttonText,
+    error,
+    success,
+  } = values;
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setValues({ ...values, buttonText: "Submitting..." });
+    const customer = {
+      username,
+      phone,
+      email,
+      address,
+      address2,
+      city,
+      state,
+      zip,
+      message,
+    };
+    //console.log(formToSubmit);
+
+    create(`http://localhost:3000/api/paintingrequest`, customer).then(
+      (data) => {
+        if (data.error) {
+          setValues({
+            ...values,
+            buttonText: "Send Message",
+            error: data.error,
+          });
+        } else {
+          setValues({
+            ...values,
+            username: "",
+            phone: "",
+            email: "",
+            address: "",
+            address2: "",
+            city: "",
+            state: "",
+            zip: "",
+            message: "",
+            buttonText: "Submitted",
+            success: data.message,
+          });
+        }
+      }
+    );
+  };
+
   return (
     <>
       <Title title="Contact" />
       <div className="jumbotron jumbotron-fluid text-center bg-white">
         <div className="container space2">
-          <div className="row" data-aos="zoom-in-down" data-aos-duration="4000">
+          <div
+            className="row"
+            //data-aos="zoom-in-down"
+            //data-aos-duration="4000"
+          >
             <div className="col-md-4">
               <p className="float-md-left">
                 <SendOutlined
@@ -52,18 +141,35 @@ function Contact(props) {
             </div>
           </div>
 
+          {error.length != 0 ? (
+            <div className="row text-danger">
+              <div className="col-12">
+                <h1>{error}</h1>
+              </div>
+            </div>
+          ) : null}
+          {success.length != 0 ? (
+            <div className="row text-success">
+              <div className="col-12">
+                <h3>{success}</h3>
+              </div>
+            </div>
+          ) : null}
+
           <div
             className="row mt-4"
-            data-aos="zoom-in-down"
-            data-aos-duration="4000"
+            //data-aos="zoom-in-down"
+            //data-aos-duration="4000"
           >
             <div className="col-12 order-2">
-              <form action="mailto:vhelen0404@gmail.com" method="post">
+              <form method="post">
                 <div className="row">
                   <div className="col-md-6 form-group">
                     <label htmlFor="name">Name/Company</label>
                     <input
                       type="text"
+                      onChange={handleChange("username")}
+                      value={username}
                       id="name"
                       className="form-control "
                     ></input>
@@ -71,7 +177,9 @@ function Contact(props) {
                   <div className="col-md-6 form-group">
                     <label htmlFor="phone">Phone</label>
                     <input
-                      type="text"
+                      type="number"
+                      onChange={handleChange("phone")}
+                      value={phone}
                       id="phone"
                       className="form-control "
                     ></input>
@@ -82,6 +190,8 @@ function Contact(props) {
                     <label htmlFor="email">Email</label>
                     <input
                       type="email"
+                      onChange={handleChange("email")}
+                      value={email}
                       id="email"
                       className="form-control "
                     ></input>
@@ -93,6 +203,8 @@ function Contact(props) {
                     <label htmlFor="inputAddress">Address</label>
                     <input
                       type="text"
+                      onChange={handleChange("address")}
+                      value={address}
                       className="form-control"
                       id="inputAddress"
                       placeholder="1234 Main St"
@@ -102,6 +214,8 @@ function Contact(props) {
                     <label htmlFor="inputAddress2">Address 2</label>
                     <input
                       type="text"
+                      onChange={handleChange("address2")}
+                      value={address2}
                       className="form-control"
                       id="inputAddress2"
                       placeholder="Apartment, studio, or floor"
@@ -114,13 +228,20 @@ function Contact(props) {
                     <label htmlFor="inputCity">City</label>
                     <input
                       type="text"
+                      onChange={handleChange("city")}
+                      value={city}
                       className="form-control"
                       id="inputCity"
                     ></input>
                   </div>
                   <div className="col-md-4 form-group">
                     <label htmlFor="inputState">State</label>
-                    <select id="inputState" className="form-control">
+                    <select
+                      id="inputState"
+                      onChange={handleChange("state")}
+                      value={state}
+                      className="form-control"
+                    >
                       <option defaultValue>Choose...</option>
                       <option>D.C.</option>
                       <option>Maryland</option>
@@ -132,6 +253,8 @@ function Contact(props) {
                     <label htmlFor="inputZip">Zip</label>
                     <input
                       type="text"
+                      onChange={handleChange("zip")}
+                      value={zip}
                       className="form-control"
                       id="inputZip"
                     ></input>
@@ -142,7 +265,8 @@ function Contact(props) {
                   <div className="col-md-12 form-group">
                     <label htmlFor="message">Description / Message</label>
                     <textarea
-                      name="message"
+                      onChange={handleChange("message")}
+                      value={message}
                       id="message"
                       className="form-control "
                       cols="30"
@@ -153,10 +277,11 @@ function Contact(props) {
                 <div className="row mt-4">
                   <div className="col-12 form-group">
                     <button
+                      onClick={handleSubmit}
                       type="button"
                       className="btn btn-primary btn-lg btn-block"
                     >
-                      Send Message
+                      {buttonText}
                     </button>
                   </div>
                 </div>
@@ -167,5 +292,6 @@ function Contact(props) {
       </div>
     </>
   );
-}
+};
+
 export default Contact;
